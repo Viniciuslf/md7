3169192970f1f8ad79db04904698b208c8f37c4d
viniciuslf <viniciuslf2205@gmail.com>
2023-05-04 14:47:16 -0300

    commit

---
+++ Lampada.java
@@ -0,0 +1,9 @@
+public class Lampada {
+    public static void main(String[] args) {
+        Status status = new Status();
+        
+        System.out.println(status.isLiga());
+     
+
+    }
+}
---
+++ Status.java
@@ -0,0 +1,21 @@
+public class Status {
+     
+    boolean liga = true;
+    boolean desliga = false;
+
+    public boolean isLiga() {
+        return liga;
+    }
+
+    public void setLiga(boolean liga) {
+        this.liga = liga;
+    }
+
+    public boolean isDesliga() {
+        return desliga;
+    }
+
+    public void setDesliga(boolean desliga) {
+        this.desliga = desliga;
+    }
+}
